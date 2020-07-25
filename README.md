077f649ba035ecc27cb7e76fd2841e3296dec75e
VallariAg <50358181+VallariAg@users.noreply.github.com>
2020-07-25 18:17:34 +0000

    Fix Pipeline errors and general editing (#1)
    1. Add .gitignore
    2. Add instructions in README.md
    3. Fix fake_news_detection.py Pipeline errors
    4. Ignore .csv files and add link to dataset in README.md

--- README.md
+++ README.md
@@ -1,5 +1,44 @@
 # Fake-News-Classifier-
-This is a fake news classifier
-Ensure that you are in the project home directory. Create the machine learning model by running below command -
-python fake_news_detection.py
-This would create a serialized version of our model into a file model.pkl
+A website to classify fake/real news by a ML model.  
+
+## Prerequisites
+
+Install `python3` and `pip`.
+
+Download dataset from [here](https://drive.google.com/file/d/1eVYHo4sowg0NBiX8aJmG8-4p_pZAXlVQ/view?usp=sharing).
+
+## Installing
+
+1. Set up virtual [virtual environment](https://docs.python.org/3/tutorial/venv.html) by running the following commands:
+    ```
+    python3 -m venv fakeNews
+    ```
+    On windows, run: 
+    ```
+    fakeNews\Scripts\activate.bat
+    ```
+    On Unix or MacOS, run:
+    ```
+    source fakeNews/bin/activate
+    ```
+
+2. Install required packages:
+    ```
+    pip install -r requirements.txt
+    ```
+
+3. To build the ML model:
+    ```
+    python3 fake_news_detection.py 
+    ```
+    This would create a serialized version of our model into a file model.pkl
+4. To test the project on locally hosted URL http://127.0.0.1:5000/ run:
+    ```
+    python3 app.py
+    ```
+
+## Authors
+
+- [Tania Malhotra](github.com/TaniaMalhotra)
+- [Malaika Agrawal]()
+- [Vallari Agrawal](github.com/vallariag)
--- fake_news_detection.py
+++ fake_news_detection.py
@@ -1,4 +1,4 @@
-#Importing the libraries
+# Importing the libraries
 import pandas as pd
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
@@ -10,9 +10,17 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 from sklearn.naive_bayes import MultinomialNB
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-df = pd.read_csv('final_news_dataset.csv', usecols = ['title', 'content', 'publication', 'label'])
-df.dropna(inplace = True)
+df = pd.read_csv('final_news_dataset.csv', usecols=[
+    'title', 'content', 'label'])
+df.dropna(inplace=True)
+
+# ----------------- Our ML model --------------------
+
 
 def convert_to_tokens(text) -> [str]:
 
@@ -22,33 +30,36 @@ def convert_to_tokens(text) -> [str]:
         words = nltk.word_tokenize(" ".join(text.split()))
 
     stop = stopwords.words('english')
-    cleanwords = [i for i in words if i not in stop and i.isalpha() and len(i) > 2]
 
     lemmatized_words = []
-    for word in cleanwords:
-        lemmatized_words.append(WordNetLemmatizer().lemmatize(word))
+    for word in words:
+        if word not in stop and word.isalpha() and len(word) > 2:
+            lemmatized_words.append(WordNetLemmatizer().lemmatize(word))
 
     return lemmatized_words
 
-def make_vocab(text, freq_limit : int) -> {}:
+
+def make_vocab(text, freq_limit: int) -> {}:
     c = Counter(convert_to_tokens(text))
     all_tokens = [word for word, count in c.items() if count > freq_limit]
 
     return all_tokens
 
-title_limit = 10
-content_limit = 200
 
-vocab_title = make_vocab(df.title, title_limit)
-vocab_content = make_vocab(df.content, content_limit)
+# title_limit = 10
+# content_limit = 200
 
-df['all_text'] = df.title + df.content
-vocab_all = set(vocab_title + vocab_content)
+# vocab_title = make_vocab(df.title, title_limit)
+# vocab_content = make_vocab(df.content, content_limit)
+
+# df['all_text'] = df.title + df.content
+# vocab_all = set(vocab_title + vocab_content)
+
 
 def extract_features(df, field, training_data, testing_data, vocabulary):
 
-    #vectorizer = TfidfVectorizer(use_idf = True, max_df = 200, stop_words = 'english')
-    vectorizer = TfidfVectorizer(use_idf = True, max_df = 200, vocabulary = vocabulary, tokenizer = convert_to_tokens)
+    vectorizer = TfidfVectorizer(
+        use_idf=True, max_df=200, vocabulary=vocabulary, tokenizer=convert_to_tokens)
     vectorizer.fit_transform(training_data[field].values)
 
     train_feature_set = vectorizer.transform(training_data[field].values)
@@ -56,75 +67,77 @@ def extract_features(df, field, training_data, testing_data, vocabulary):
 
     return train_feature_set, test_feature_set, vectorizer
 
-def pre_training(df, field, vocabulary) :
 
-    training_data, testing_data = train_test_split(df, test_size = 0.30, random_state = 2000)
+def pre_training(df, field, vocabulary):
+
+    training_data, testing_data = train_test_split(
+        df, test_size=0.30, random_state=2000)
 
     Y_train = training_data['label'].values
     Y_test = testing_data['label'].values
 
-    X_train, X_test, feature_transformer = extract_features(df, field, training_data, testing_data, vocabulary)
+    X_train, X_test, feature_transformer = extract_features(
+        df, field, training_data, testing_data, vocabulary)
 
     return X_train, X_test, Y_train, Y_test, feature_transformer
 
 
-
 def train_bayes_model(df, field, vocabulary):
 
-  X_train, X_test, Y_train, Y_test, feature_transformer = pre_training(df, field, vocabulary)
-  mnb = MultinomialNB()
-  mnb.fit(X_train, Y_train)
-  y_pred = mnb.predict(X_test)
+    X_train, X_test, Y_train, Y_test, feature_transformer = pre_training(
+        df, field, vocabulary)
+    mnb = MultinomialNB()
+    mnb.fit(X_train, Y_train)
+    y_pred = mnb.predict(X_test)
 
-  return feature_transformer, y_pred, Y_test
-
+    return feature_transformer, y_pred, Y_test
 
 
 def train_SVM_model(df, field, vocab):
 
-  svm_cf = svm.SVC(kernel = 'rbf')
-  X_train, X_test, Y_train, Y_test, feature_transformer = pre_training(df, field, vocab)
-  svm_cf.fit(X_train, Y_train)
-  y_pred = svm_cf.predict(X_test)
+    svm_cf = svm.SVC(kernel='rbf')
+    X_train, X_test, Y_train, Y_test, feature_transformer = pre_training(
+        df, field, vocab)
+    svm_cf.fit(X_train, Y_train)
+    y_pred = svm_cf.predict(X_test)
 
-  return feature_tranformer, y_pred, Y_test
-
+    return feature_transformer, y_pred, Y_test
 
 
 def train_LR_model(df, field, vocabulary):
 
-    X_train, X_test, Y_train, Y_test, feature_transformer = pre_training(df, field, vocabulary)
-    log_reg = LogisticRegression(solver = 'sag', random_state = 0, C = 5, max_iter = 1000)
+    X_train, X_test, Y_train, Y_test, feature_transformer = pre_training(
+        df, field, vocabulary)
+    log_reg = LogisticRegression(
+        solver='sag', random_state=0, C=5, max_iter=1000)
     log_reg.fit(X_train, Y_train)
     y_pred = log_reg.predict(X_test)
 
     return feature_transformer, y_pred, Y_test
 
+# ---------------------------------------------------
+
+# --------- Model used to test website --------------
 
 
+df['text'] = df['title'] + df['content']
+X = df['text']
+y = df['label']
 
-#Importing the cleaned file containing the text and label
-#news = pd.read_csv('news.csv')
-#X = news['text']
-#y = news['label']
+# Splitting the data into train
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# Creating a pipeline that first creates bag of words(after applying stopwords) & then applies Multinomial Naive Bayes model
+pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
+                     ('nbmodel', MultinomialNB())])
+# Training our data
+pipeline.fit(X_train, y_train)
+# Predicting the label for the test data
+pred = pipeline.predict(X_test)
+# Checking the performance of our model
+print(classification_report(y_test, pred))
+print(confusion_matrix(y_test, pred))
+# ---------------------------------------------------
 
-#Splitting the data into train
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-#Creating a pipeline that first creates bag of words(after applying stopwords) & then applies Multinomial Naive Bayes model
-#pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
-#                    ('nbmodel', MultinomialNB())])
-
-#Training our data
-#pipeline.fit(X_train, y_train)
-
-#Predicting the label for the test data
-#pred = pipeline.predict(X_test)
-
-#Checking the performance of our model
-#print(classification_report(y_test, pred))
-#print(confusion_matrix(y_test, pred))
-
-#Serialising the file
+# Serialising the file
 with open('model.pickle', 'wb') as handle:
-    pickle.dump(handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(pipeline, handle, protocol=pickle.HIGHEST_PROTOCOL)
