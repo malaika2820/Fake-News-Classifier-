5f10731f363216c1f4ce007860735027e47d6fb6
Tania Malhotra <62426189+TaniaMalhotra@users.noreply.github.com>
2020-07-27 23:19:18 +0530

    Imported function for pre-processing from fake_news_detection.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# Importing the Libraries
-import numpy as np
-from flask import Flask, request, render_template
+from fake_news_detection import extract_features_for_scraped_input
 from flask_cors import CORS
 import os
 import joblib
@@ -9,38 +7,40 @@ import flask
 import os
 import newspaper
 from newspaper import Article
+from flask import Flask, request,render_template
 import urllib
 import nltk
-nltk.download('punkt')
 
-# Loading Flask and assigning the model variable
+
+mnb = joblib.load('model.pkl') 
+
+#Loading Flask and assigning the model variable
 app = Flask(__name__)
 CORS(app)
-app = flask.Flask(__name__, template_folder='templates')
-
-with open('model.pickle', 'rb') as handle:
-    model = pickle.load(handle)
-
+app=flask.Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def main():
     return render_template('main.html')
 
-# Receiving the input url from the user and using Web Scrapping to extract the news content
-@app.route('/predict', methods=['GET', 'POST'])
+
+
+
+#Receiving the input url from the user and using Web Scrapping to extract the news content
+@app.route('/predict',methods=['GET','POST'])
 def predict():
-    url = request.get_data(as_text=True)[5:]
+    url =request.get_data(as_text=True)[5:]
     url = urllib.parse.unquote(url)
     article = Article(str(url))
     article.download()
     article.parse()
     article.nlp()
-    news = article.summary
-    # Passing the news article to the model and returing whether it is Fake or Real
-    pred = model.predict([news])
-    return render_template('main.html', prediction_text='The news is "{}"'.format(pred[0]))
+    news = [article.summary]
+    pred = mnb.predict(extract_features_for_scraped_input(news))
+    print(pred)
+    return render_template('main.html', prediction_text='The news is {}'.format(pred))
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(port=port, debug=True, use_reloader=False)
+if __name__=="__main__":
+    port=int(os.environ.get('PORT',5000))
+    app.run(port=port,debug=True,use_reloader=False)
